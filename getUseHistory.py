d96b007c529d8762da1d09bc693e05af75055ea9
AlBan93 <albaneo0724@gmail.com>
2023-03-21 12:20:18 +0900

    함수 호출 오류 수정 및 누락된 파라미터 추가

---
+++ getUseHistory.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import imp
+import sys
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding("UTF8")
+except Exception as E:
+    pass
+
+import testValue
+from popbill import HTTaxinvoiceService, PopbillException
+
+htTaxinvoiceService = HTTaxinvoiceService(testValue.LinkID, testValue.SecretKey)
+htTaxinvoiceService.IsTest = testValue.IsTest
+htTaxinvoiceService.IPRestrictOnOff = testValue.IPRestrictOnOff
+htTaxinvoiceService.UseStaticIP = testValue.UseStaticIP
+htTaxinvoiceService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+"""
+연동회원의 포인트 사용내역을 확인합니다.
+- https://developers.popbill.com/reference/htcashbill/python/api/point#GetUseHistory
+"""
+
+try:
+    print("=" * 15 + " 포인트 사용내역 확인 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 조회 기간의 시작일자 (형식 : yyyyMMdd)
+    SDate = "20230101"
+
+    # 조회 기간의 종료일자 (형식 : yyyyMMdd)
+    EDate = "20230131"
+
+    # 목록 페이지번호 (기본값 1)
+    Page = 1
+
+    # 페이지당 표시할 목록 개수 (기본값 500, 최대 1,000)
+    PerPage = 500
+
+    # 거래일자를 기준으로 하는 목록 정렬 방향 ("D"(기본값), "A" 중 택 1)
+    Order = "D"
+
+    # 팝빌회원 팝빌 아이디
+    UserID = testValue.testUserID
+
+    useHistoryResult = htTaxinvoiceService.getUseHistory(
+        # 팝빌회원 사업자번호
+        CorpNum,
+        # 조회 기간의 시작일자
+        SDate,
+        # 조회 기간의 종료일자
+        EDate,
+        # 목록 페이지번호
+        Page,
+        # 페이지당 표시할 목록 개수
+        PerPage,
+        # 거래일자를 기준으로 하는 목록 정렬 방향
+        Order,
+        # 팝빌회원 아이디
+        UserID,
+    )
+
+    print(" code (요청에 대한 응답 상태 코드) : %s" % useHistoryResult.code)
+    print(" total (총 검색결과 건수) : %s" % useHistoryResult.total)
+    print(" perPage (페이지당 검색 개수) : %s" % useHistoryResult.perPage)
+    print(" pageNum (페이지 번호) : %s" % useHistoryResult.pageNum)
+    print(" pageCount (페이지 개수) : %s" % useHistoryResult.pageCount)
+
+    for useHistory in useHistoryResult.list:
+        print(" itemCode (서비스코드) : %s" % useHistory.itemCode)
+        print(" txType (포인트 증감 유형) : %s" % useHistory.txType)
+        print(" txPoint (증감 포인트) : %s" % useHistory.txPoint)
+        print(" balance (잔여포인트) : %s" % useHistory.balance)
+        print(" txDT (포인트 증감 일시) : %s" % useHistory.txDT)
+        print(" userID (담당자 아이디) : %s" % useHistory.userID)
+        print(" userName (담당자명) : %s" % useHistory.userName)
+        print("*" * 50)
+
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
---
+++ paymentRequest.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import imp
+import sys
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding("UTF8")
+except Exception as E:
+    pass
+
+import testValue
+from popbill import HTTaxinvoiceService, PaymentForm, PopbillException
+
+htTaxinvoiceService = HTTaxinvoiceService(testValue.LinkID, testValue.SecretKey)
+htTaxinvoiceService.IsTest = testValue.IsTest
+htTaxinvoiceService.IPRestrictOnOff = testValue.IPRestrictOnOff
+htTaxinvoiceService.UseStaticIP = testValue.UseStaticIP
+htTaxinvoiceService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+"""
+연동회원 포인트 충전을 위해 무통장입금을 신청합니다.
+- https://developers.popbill.com/reference/htcashbill/python/api/point#PaymentRequest
+"""
+
+try:
+    print("=" * 15 + " 무통장 입금신청 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 입금신청 객체정보
+    paymentForm = PaymentForm(
+        settleName="담당자",  # 담당자명
+        settlerEmail="settler@Email.com",  # 담당자 이메일
+        notifyHP="01012341234",  # 담당자 휴대폰
+        paymentName="입금자이름",  # 입금자명
+        settleCost="10000",  # 결제금액
+    )
+
+    # 팝빌회원 팝빌 아이디
+    UserID = testValue.testUserID
+
+    paymentResponse = htTaxinvoiceService.paymentRequest(CorpNum, paymentForm, UserID)
+
+    print("code (응답코드) : %s" % paymentResponse.code)
+    print("message (응답메시지) : %s" % paymentResponse.message)
+    print("settleCode (정산코드) : %s" % paymentResponse.settleCode)
+
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
