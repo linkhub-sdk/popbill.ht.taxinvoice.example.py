a9b2b0803a6962164a04c2d8fd527a09fe564137
AlBan93 <albaneo0724@gmail.com>
2023-05-11 17:31:34 +0900

    주석 포맷 및 버그 수정

--- getRefundInfo.py
+++ getRefundInfo.py
@@ -19,7 +19,7 @@ htTaxinvoiceService.UseStaticIP = testValue.UseStaticIP
 htTaxinvoiceService.UseLocalTimeYN = testValue.UseLocalTimeYN
 
 """
- * 포인트 환불에 대한 상세정보 1건을 확인합니다.
+포인트 환불에 대한 상세정보 1건을 확인합니다.
 - https://developers.popbill.com/reference/httaxinvoice/python/api/point#GetRefundInfo
 """
 
